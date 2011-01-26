676af29668cba9fd79c71b6e11f322f05d0381d6
David Mortensen <davidmortensen@gmail.com>
2011-01-25 19:52:10 -0500

    Generalized install script.

--- install.sh
+++ install.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
 
-mkdir -p /Library/WebServer/Documents/webcomparator
-mkdir -p /Library/WebServer/Documents/webcomparator/js
-mkdir -p /Library/WebServer/Documents/webcomparator/js/
-mkdir -p /Library/WebServer/Documents/webcomparator/css
-cp  html/*.html /Library/WebServer/Documents/webcomparator/
-cp  html/js/*.js /Library/WebServer/Documents/webcomparator/js/
-cp -r  html/js/i18n /Library/WebServer/Documents/webcomparator/js/
-cp  html/css/*.css /Library/WebServer/Documents/webcomparator/css/
-cp dist/build/query.cgi/query.cgi /Library/WebServer/CGI-Executables/
-cp dist/build/edit.cgi/edit.cgi /Library/WebServer/CGI-Executables/
-cp data/Tangkhul3.db /tmp
-chmod a+rw /tmp/Tangkhul3.db
+CGIBIN = /Library/WebServer/CGI-Executables
+CGI = /Library/WebServer/Documents
+DB = /var/webcomparator
+
+mkdir -p $CGI/webcomparator
+mkdir -p $CGI/webcomparator/js
+mkdir -p $CGI/webcomparator/css
+cp  html/*.html $CGI/webcomparator/
+cp  html/js/*.js $CGI/webcomparator/js/
+cp -r  html/js/i18n $CGI/webcomparator/js/
+cp  html/css/*.css $CGI/webcomparator/css/
+cp dist/build/query.cgi/query.cgi $CGIBIN
+cp dist/build/edit.cgi/edit.cgi $CGIBIN
+mkdir -p $DB
+cp data/Tangkhul3.db $DB
+chmod a+rw $DB/Tangkhul3.db
